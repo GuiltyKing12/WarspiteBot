31a5f667ffd7c6d6369e34beedeafaed76438747
GuiltyKing12 <bher@mines.edu>
2016-10-19 17:33:00 -0600

    Fixed a quick wrong add

--- lib/warspite_bot.rb
+++ lib/warspite_bot.rb
@@ -4,8 +4,10 @@ require 'yaml'
 
 # main bot module
 module WarspiteBot
+  # load in all the needed files for commands, config, and events
   Dir["#{File.dirname(__FILE__)}/warspite_bot/*.rb"].each { |file| require file }
 
+  # initialize -> gets the information from config file
   CONFIG = Config.new
 
   BOT = Discordrb::Commands::CommandBot.new(
